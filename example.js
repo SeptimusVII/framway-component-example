d7d4b171603d8460cd62d5a593b2e6ce505a9374
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-09-24 15:51:59 +0200

    add loading message

--- example.js
+++ example.js
@@ -5,7 +5,7 @@ module.exports = function(app){
     Example.lastUpdate     = "2.0.0";
     Example.version        = "1";
     // Example.factoryExclude = true;
-    // Example.loadingMsg     = "This message will display in the console when component will be loaded.";
+    Example.loadingMsg     = "Example component, used as a syntax and structure model. Do not use in production.";
     // Example.requires       = [];
 
     // Example.prototype.onCreate = function(){
